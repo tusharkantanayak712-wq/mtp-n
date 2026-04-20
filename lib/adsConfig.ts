f81e52c2cccd6d0124661f8c76dd8491a12a5c81
tushar-kant <tusharkantanayak713@gmail.com>
2026-04-20 22:27:01 +0530

    fix

--- lib/adsConfig.ts
+++ lib/adsConfig.ts
@@ -1,9 +1,9 @@
 export const ADS_CONFIG = {
-  ADSTERRA_LINK: "https://www.profitablecpmratenetwork.com/hufhc5xjdq?key=061e505a28e2947339287dabe62e6175",
+  ADSTERRA_LINK: "https://omg10.com/4/10900401",
   WATCH_EARN_CHANNELS: [
     { id: "watch_1", title: "Channel 1", link: "https://www.profitablecpmratenetwork.com/xwgw5tfxdb?key=30e58d6b2ddb7c342e339e0357a148c6", reward: 1, cooldownMs: 35 * 60 * 1000 },
     { id: "watch_2", title: "Channel 2", link: "https://www.profitablecpmratenetwork.com/f1p10c8uc3?key=0d7879fcf35dc241e476f2e895fabdbc", reward: 1, cooldownMs: 45 * 60 * 1000 },
-    { id: "watch_3", title: "Channel 3", link: "https://omg10.com/4/10900401", reward: 1, cooldownMs: 40 * 60 * 1000 },
+    { id: "watch_3", title: "Channel 3", link: "https://www.profitablecpmratenetwork.com/hufhc5xjdq?key=061e505a28e2947339287dabe62e6175", reward: 1, cooldownMs: 40 * 60 * 1000 },
     { id: "watch_4", title: "Channel 4", link: "https://omg10.com/4/9869073", reward: 1, cooldownMs: 35 * 60 * 1000 },
     { id: "watch_5", title: "Channel 5", link: "https://www.profitablecpmratenetwork.com/na3uf7p2jk?key=88e14dd1c1b8f2d96d5474d1a62b0bec", reward: 1, cooldownMs: 45 * 60 * 1000 },
   ],
